de1bb36ddc9f6cd1d85f90a42024c8925270dca0
Evgenii <spas19@yandex.ru>
2018-10-18 18:35:09 +0300

    ver.1

---
+++ src/Lessons_1/Zadacha_1.java
@@ -0,0 +1,18 @@
+package Lessons_1;
+
+/*
+Задайте высоту,длину и ширину прямоугольного паралепипеда.
+Найти:
+1. его площадь;
+2. узнайте что больше: ширина или высота и выведите информацию об это в консоль
+ */
+
+public class Zadacha_1 {
+    public static void main(String[] args) {
+        int height = 2;
+        int lenth = 5;
+        int width = 3;
+        System.out.println("Площадь прямоугльника = " + lenth * width);
+        System.out.println((width > height) ? "ширина больше высоты" : "высота больше ширины");
+    }
+}
