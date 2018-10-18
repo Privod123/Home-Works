fa88ec4d01b58cd35f949a67de972958cb8ebf51
Evgenii <spas19@yandex.ru>
2018-10-18 18:38:31 +0300

    ver.1

---
+++ src/Lessons_1/Zadacha_2.java
@@ -0,0 +1,24 @@
+package Lessons_1;
+
+/*
+На садовом участке площадью 10 соток,разбили грядки 15 на 25 метров.
+Сколько м2 осталось не занято?
+ */
+
+
+public class Zadacha_2 {
+    public static void main(String[] args) {
+        int squareGarden = 100;
+        int lenthGardenBed = 25;
+        int widthGadenBad = 15;
+        // вычислим площать участка в м2.
+        int squareGardenMetri = squareGarden * 100;
+        // вычислим площадь одной грядки
+        int squareGardenBad = lenthGardenBed * widthGadenBad;
+        // найдем сколько целых грядок поместиться на участке
+        int amountGardenBad = squareGardenMetri/squareGardenBad;
+        // найдем сколько осталось свободного места
+        int freeLandGarden = squareGardenMetri - amountGardenBad * squareGardenBad;
+        System.out.println("Осталось не занято : " + freeLandGarden + " м2");
+    }
+}
