5d884cd47b56ff99fc09595de5bddf201f16697e
Evgenii <spas19@yandex.ru>
2018-10-20 20:42:57 +0300

    ver.1

---
+++ src/Lessons_2/Zadacha_3.java
@@ -0,0 +1,14 @@
+package Lessons_2;
+/*
+Создайте программу,выводящую на экран первые 20 элементов последовательности
+2 4 8 16 32 64 128 ...
+ */
+
+public class Zadacha_3 {
+    public static void main(String[] args) {
+        int a = 2;
+        for (int i = 1; i <= 20; i++) {
+            System.out.print ((int) Math.pow(a,i) + " ");
+        }
+    }
+}
