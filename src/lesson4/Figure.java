c09e7c5be914e3a175f522d4727667d1099af6fe
Evgenii <spas19@yandex.ru>
2018-10-25 18:03:00 +0300

    ver.1 Zadachi_1 lesson4

---
+++ src/lesson4/Figure.java
@@ -0,0 +1,55 @@
+package lesson4;
+
+public class Figure {
+    final double pi = 3.14;
+    private double radiys;
+    private double sideA;
+    private double sideB;
+    private double sideC;
+    private double squareCircle;
+    private double perimeterCircle;
+    private double squareRectangle;
+    private double perimeterRectangle;
+    private double squareTriagle;
+    private double perimeterTriagle;
+
+    public Figure(double radiys) {
+        this.radiys = radiys;
+    }
+
+    public Figure(double sideA, double sideB) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+    }
+
+    public Figure(double sideA, double sideB, double sideC) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+    }
+
+    public double getSquareCircle() {
+        return pi*Math.pow(radiys,2);
+    }
+
+    public double getPerimeterCircle() {
+        return 2*pi*radiys;
+    }
+
+    public double getSquareRectangle() {
+        return sideA * sideB;
+    }
+
+    public double getPerimeterRectangle() {
+        return 2*(sideA + sideB);
+    }
+
+    public double getSquareTriagle() {
+        double p = (sideA + sideB + sideC)/2;
+        return Math.sqrt(p*(p - sideA)*(p - sideB)*(p - sideC));
+    }
+
+    public double getPerimeterTriagle() {
+        return sideA + sideB + sideC;
+    }
+}
---
+++ src/lesson4/TestFigure.java
@@ -0,0 +1,23 @@
+package lesson4;
+/*
+Создайте фигуры: Circle,Rectangle,Triagle, у которых будут координаты. Все фигуры должны иметь методы которые
+возвращают площадь и периметр(для окружности - длина окружности).
+ */
+
+public class TestFigure {
+    public static void main(String[] args) {
+        System.out.println("");
+        Figure circle = new Figure(2);
+        System.out.println("Площадь круга - " + circle.getSquareCircle());
+        System.out.println("Периметр круга - " + circle.getPerimeterCircle());
+        System.out.println("--------------------------------------");
+        Figure rectangle = new Figure(2,3);
+        System.out.println("Площадь прямоугольника - " + rectangle.getSquareRectangle());
+        System.out.println("Периметр прямоугольника - " + rectangle.getPerimeterRectangle());
+        System.out.println("--------------------------------------");
+        Figure triagle = new Figure(3,4,5);
+        System.out.println("Площадь треугольника - " + triagle.getSquareTriagle());
+        System.out.println("Периметр треугольника - " + triagle.getPerimeterTriagle());
+
+        }
+}
