ae1775df82f2051199a98382bd05feb8f8dc595a
Tropin Evgenii <spas24@mail.ru>
2018-11-14 16:49:31 +0300

    ver 2 Zadacha_2 lessons11 Collection

--- src/lessons11/zadachiCollection/Zadacha2.java
+++ src/lessons11/zadachiCollection/Zadacha2.java
@@ -39,7 +39,7 @@ public class Zadacha2 {
         }
     }
 
-    static void printList(ArrayList<Integer> list1,ArrayList<Integer> list2,
+    void printList(ArrayList<Integer> list1,ArrayList<Integer> list2,
                           ArrayList<Integer> list3,ArrayList<Integer> list4) {
         System.out.println("Выводим полный список");
         for (Integer i : list1) {
@@ -58,15 +58,30 @@ public class Zadacha2 {
             System.out.println(i);
         }
     }
-    static void printList(ArrayList<Integer> listName){
+    void printList(ArrayList<Integer> listName){
         System.out.println("Выводим запрощенный список");
         for (Integer i : listName) {
             System.out.println(i);
         }
     }
+    void printList(String listName){
+        System.out.println("Выводим список с именем " + listName);
+        if (listName.equals("%3")){
+            printList(listDeleniaNa3);
+        }else  if (listName.equals("%2")){
+            printList(listDeleniaNa2);
+        } else if (listName.equals("general")){
+            printList(generalList);
+        } else if (listName.equals("other")){
+            printList(otherList);
+        } else {
+            System.out.println("Списка с именем " + listName + " нет");
+        }
+    }
     public static void main(String[] args) {
         Zadacha2 listZadacha = new Zadacha2();
-        printList(listZadacha.generalList,listZadacha.listDeleniaNa3,listZadacha.listDeleniaNa2,listZadacha.otherList);
-        printList(listZadacha.otherList);
+        listZadacha.printList(listZadacha.generalList,listZadacha.listDeleniaNa3,listZadacha.listDeleniaNa2,listZadacha.otherList);
+        listZadacha.printList(listZadacha.otherList);
+        listZadacha.printList("%3");
     }
 }
